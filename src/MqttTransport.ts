43221e16912dabe09608db2d263386266936976d
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-02-07 13:52:55 -0300

    feat: Extended MQTTTransport functionalities and updated configurations

--- src/MqttTransport.ts
+++ src/MqttTransport.ts
@@ -4,12 +4,16 @@ type Options = {
   debug: boolean
   log: any[]
   client: IClientOptions
+  subTopic: string
+  pubTopic: string
+  qos?: 0 | 1 | 2
 }
 
 type Config = {
   subTopic: string
   pubTopic: string
   qos?: 0 | 1 | 2
+  msg: object
 }
 
 // export type MQTTTransportOptions = Partial<Options>
@@ -24,14 +28,42 @@ const defaults: Options = {
     host: '',
     port: 1883,
   },
+  subTopic: '',
+  pubTopic: '',
+  qos: 0,
 }
 
 function MQTTTransport(this: any, options: Options) {
   const seneca: any = this
+
+  const tag = seneca.plugin.tag
+  const gtag = null == tag || '-' === tag ? '' : '$' + tag
+  const gateway = seneca.export('gateway' + gtag + '/handler')
+
+  // const log = options.debug && (options.log || [])
+  // const tu = seneca.export('transport/utils')
+
+  const subTopic = options.subTopic
+  // const pubTopic = options.pubTopic
   const client: MqttClient = connect(options.client)
 
   client.on('connect', function () {
     console.log('Connected to the broker')
+
+    if (subTopic) {
+      client.subscribe(subTopic, { qos: 0 }, (err) => {
+        if (err) {
+          console.error('Subscribe error: ', err)
+        }
+      })
+
+      client.on('message', (topic, message) => {
+        seneca.act('role:transport,hook:listen,type:mqtt', {
+          msg: JSON.parse(message.toString()),
+          subTopic: topic,
+        })
+      })
+    }
   })
 
   client.on('error', function (err: any) {
@@ -39,49 +71,64 @@ function MQTTTransport(this: any, options: Options) {
   })
 
   seneca.add('role:transport,hook:listen,type:mqtt', hook_listen_mqtt)
-  seneca.add('role:transport,hook:client,type:mqtt', hook_client_mqtt)
+  // seneca.add('role:transport,hook:client,type:mqtt', hook_client_mqtt)
 
   function hook_listen_mqtt(this: any, config: Config, ready: Function) {
+    const msg = config.msg
     const subTopic = config.subTopic
-    const qos = config.qos || 0
-
-    client.subscribe(subTopic, { qos }, (err) => {
-      if (err) {
-        console.error('Subscribe error: ', err)
-        return ready({ err })
-      }
-    })
-
-    client.on('message', (topic, message) => {
-      if (subTopic == topic) {
-        const msg = JSON.parse(message.toString())
-        return ready(msg)
-      }
-    })
-  }
-
-  async function hook_client_mqtt(this: any, config: Config, ready: Function) {
-    const pubTopic = config.pubTopic
-    const qos = config.qos || 0
-
-    async function pub_msg(msg: any, reply: any) {
-      const msgStr = JSON.stringify(msg)
-
-      client.publish(pubTopic, msgStr, { qos }, (err: any) => {
-        if (err) {
-          console.error('Publish error: ', err)
-          reply({ ok: false, err })
-        }
-
-        reply({ ok: true, msg })
-      })
+    const action = {
+      role: 'transport',
+      cmd: 'subscribed',
+      topic: subTopic,
+      ...msg,
     }
 
-    return ready({
-      config: config,
-      pub: pub_msg,
-    })
+    return gateway(action, { local: true })
+      .then(function (out: any) {
+        ready(null, out)
+      })
+      .catch(function (err: any) {
+        ready({ error: err })
+      })
   }
+
+  // async function hook_client_mqtt(this: any, config: Config, ready: Function) {
+  //   const pubTopic = config.pubTopic
+  //   const qos = config.qos || 0
+  //
+  //   async function send_msg(msg: any, reply: any, meta: any) {
+  //     const msgstr = JSON.stringify(tu.externalize_msg(seneca, msg, meta))
+  //     log &&
+  //       log.push({
+  //         hook: 'client',
+  //         entry: 'send',
+  //         pat: meta.pattern,
+  //         w: Date.now(),
+  //         m: meta.id,
+  //       })
+  //
+  //     let ok = false
+  //     let sent = null
+  //     let err = null
+  //
+  //     client.publish(pubTopic, msgstr, { qos }, (e: any) => {
+  //       if (e) {
+  //         err = e
+  //         console.log('MQTT SENT ERROR', e)
+  //       }
+  //
+  //       ok = true
+  //       sent = true
+  //     })
+  //
+  //     reply({ ok, sent, msgstr, err })
+  //   }
+  //
+  //   return ready({
+  //     config: config,
+  //     send: send_msg,
+  //   })
+  // }
 
   return {
     exports: {},
