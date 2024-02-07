7ce6f852bfb0e0bef0da99b1a6a7847445e25664
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-02-07 15:09:35 -0300

    test: improves error reporting and provides more context during both connection and message publication stages.

--- test/quick.js
+++ test/quick.js
@@ -6,6 +6,8 @@ const testSubTopic = 'test/quick'
 //Public host - don't send sensitive data
 const testHost = 'test.mosquitto.org'
 
+run()
+
 async function run() {
   await Seneca({ legacy: false, timeout: 1111 })
     .test('print')
@@ -19,41 +21,40 @@ async function run() {
       },
       subTopic: testSubTopic,
     })
-    .message(
-      'role:transport,cmd:subscribed,topic:test/quick',
-      async function (msg) {
-        if (msg && msg.x && msg.y) {
-          return { result: msg.x + msg.y }
-        } else {
-          return { error: 'Missing or invalid message data' }
-        }
-      },
-    )
-    .listen()
+    .message('role:transport,cmd:sub,topic:test/quick', async function (msg) {
+      if (msg && msg.x && msg.y) {
+        return { result: msg.x + msg.y }
+      } else {
+        return { error: 'Missing or invalid message data' }
+      }
+    })
+    .listen({ type: 'mqtt' })
     .ready()
-}
 
-run()
-
-// Simulating an external mqtt client
-const client = connect({
-  host: testHost,
-  protocol: 'mqtt',
-  port: 1883,
-})
-
-client.on('connect', function () {
-  const messageAction = { x: 5, y: 2 }
-
-  const messageStr = JSON.stringify(messageAction)
-  client.publish(testSubTopic, messageStr, (err) => {
-    if (err) {
-      console.error(err)
-    }
-    console.log('Published message to the MQTT broker', messageStr)
+  // Simulating an external mqtt client
+  const client = connect({
+    host: testHost,
+    protocol: 'mqtt',
+    port: 1883,
   })
-})
 
-client.on('error', function (err) {
-  console.error('Connection error: ', err)
-})
+  client.on('connect', function () {
+    console.log('External Connected to the broker')
+    const messageAction = { x: 5, y: 2 }
+    const messageStr = JSON.stringify(messageAction)
+
+    client.publish(testSubTopic, messageStr, (err) => {
+      if (err) {
+        console.error(err)
+      }
+      console.log(
+        'External Client Published message to the MQTT broker',
+        messageStr,
+      )
+    })
+  })
+
+  client.on('error', function (err) {
+    console.error('Connection error: ', err)
+  })
+}
